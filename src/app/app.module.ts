252ed3a65069df871f0ad631a2000aa05ebd62bd
Dagoberto <dagorodri93@gmail.com>
2019-08-23 13:29:11 -0500

    finished the functionality of the basic app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "angularfire2";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { environment } from "src/environments/environment";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { HeaderComponent } from "./header/header.component";
+
+import { MatToolbarModule } from "@angular/material";
+import { CustomerDetailsComponent } from "./customers/customer-details/customer-details.component";
+import { CustomersListComponent } from "./customers/customers-list/customers-list.component";
+import { CreateCustomerComponent } from "./customers/create-customer/create-customer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CustomerDetailsComponent,
+    CustomersListComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
